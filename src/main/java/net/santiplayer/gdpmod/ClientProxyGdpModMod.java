877b16b85add59fc4ebf40d365069a1ce62ec2d0
Santiplayer18 <scano@SP18-PC>
2023-04-11 19:13:48 +0200

    Archivos esenciales del mod

---
+++ src/main/java/net/santiplayer/gdpmod/ClientProxyGdpModMod.java
@@ -0,0 +1,26 @@
+package net.santiplayer.gdpmod;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxyGdpModMod implements IProxyGdpModMod {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("gdp_mod");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
