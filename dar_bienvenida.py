da278fec54a7a60c752cb0c96ce145e3df0b42b2
Santiago Cano Muélledes <43007481+Santiplayer18@users.noreply.github.com>
2023-05-13 17:09:04 +0200

    Actualización a la última versión

--- dar_bienvenida.py
+++ dar_bienvenida.py
@@ -1,13 +1,11 @@
 import tkinter as tk
 
+# Saludo al usuario y espera a confirmación para continuar
 menu_bienvenida = tk.Tk()
-menu_bienvenida.geometry("500x100")
+menu_bienvenida.geometry("500x80")
 menu_bienvenida.title("GdP Installer")
 
-texto = tk.Label(menu_bienvenida, text="¡Bienvenido a GdP Installer! Cuando desee comenzar con la instalación, pulse 'Continuar'")
-texto.pack(pady=10)
-
-boton = tk.Button(menu_bienvenida, text="Continuar", command=lambda: menu_bienvenida.destroy())
-boton.pack(pady=10)
+tk.Label(menu_bienvenida, text="¡Bienvenido a GdP Installer! Cuando desee comenzar con la instalación, pulse 'Continuar'").pack(pady=10)
+tk.Button(menu_bienvenida, text="Continuar", command=lambda: menu_bienvenida.destroy()).pack()
 
 menu_bienvenida.mainloop()
