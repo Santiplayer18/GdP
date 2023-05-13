70965581d0ce9f46dd0ef11fc7deb57784fd7f2b
Santiago Cano Muélledes <43007481+Santiplayer18@users.noreply.github.com>
2023-05-13 17:16:08 +0200

    Actualización a la última versión

--- desinstalar_java.py
+++ desinstalar_java.py
@@ -1,3 +1,8 @@
-import os
+import subprocess
+import sys
 
-os.system('wmic product where "name like \'%%IBM Semeru%%\' and name like \'%%8u%%\'" call uninstall /nointeractive')
+# Búsqueda y desinstalación de todas las versiones de Java 8 en el sistema
+try:
+    subprocess.check_call(["wmic", "product", "where", "name like '%IBM Semeru%' and name like '%8u%'", "call", "uninstall", "/nointeractive"])
+except subprocess.CalledProcessError:
+    sys.exit()
