d97f30555093f1537689e3d9e91ec601755c72aa
Santiago Cano Muélledes <43007481+Santiplayer18@users.noreply.github.com>
2023-05-13 17:25:28 +0200

    Actualización a la última versión

--- determinar_admin.py
+++ determinar_admin.py
@@ -3,31 +3,22 @@ import os
 import ctypes
 import sys
 
+# Determinación de privilegios de administrador
 try:
- admin = os.getuid() == 0
+    admin = os.getuid() == 0
 except AttributeError:
- admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-
-if admin == True:
-    menu_admin = tk.Tk()
-    menu_admin.geometry("400x90")
-    menu_admin.title("GdP Installer")
-
-    texto = tk.Label(menu_admin, text="El programa posee permisos de administrador")
-    texto.pack(pady=10)
-
-    menu_admin.mainloop()
-    sys.exit()
-else :
+    admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+ 
+if not admin:
     menu_noadmin = tk.Tk()
     menu_noadmin.geometry("400x90")
     menu_noadmin.title("GdP Installer")
 
-    texto1 = tk.Label(menu_noadmin, text="El programa no puede continuar al carecer de permisos de administrador")
-    texto1.pack(pady=10)
+    tk.Label(menu_noadmin, text="El programa no puede continuar al carecer de permisos de administrador").pack(pady=10)
+    tk.Label(menu_noadmin, text="Por favor, vuélvalo a intentar con los permisos necesarios").pack(pady=10)
 
-    texto2 = tk.Label(menu_noadmin, text="Por favor, vuélvalo a intentar con los permisos necesarios")
-    texto2.pack(pady=10)
-
+    logging.error("Privilegios de administrador no detectados")
     menu_noadmin.mainloop()
+
+    logging.error("Error fatal. Forzada finalización del programa")
     sys.exit()
