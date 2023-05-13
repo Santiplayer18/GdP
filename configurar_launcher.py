8f1b08add171e9b0dc38b24f46acc445b4e4e2b5
Santiago Cano Muélledes <43007481+Santiplayer18@users.noreply.github.com>
2023-05-13 20:15:20 +0200

    Fusión con preguntar_admin.py para hacerlo usable

--- configurar_launcher.py
+++ configurar_launcher.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import os
 import psutil
 import zipfile
@@ -35,6 +36,24 @@ def crear_acceso(directorio_diana, directorio_acceso):
 
     persist_file = shell_link.QueryInterface(wintypes.IID_IPersistFile)
     persist_file.Save(directorio_acceso, True)
+    
+# Pregunta para dividir usuarios premium y no premium
+menu_preguntamc = tk.Tk()
+menu_preguntamc.geometry("300x90")
+menu_preguntamc.title("GdP Installer")
+
+tk.Label(menu_preguntamc, text="¿Actualmente posee Minecraft: Java Edition?").pack(pady=10)
+
+mcpremium = tk.BooleanVar()
+mcpremium.set(False)
+
+marco_botones = tk.Frame(menu_preguntamc)
+marco_botones.pack()
+
+tk.Button(marco_botones, text="Sí", command=lambda: (mcpremium.set(True), menu_preguntamc.destroy())).pack(side=tk.LEFT, padx=10)
+tk.Button(marco_botones, text="No", command=lambda: (menu_preguntamc.destroy())).pack(side=tk.LEFT, padx=10)
+
+menu_preguntamc.mainloop()
     
 # Selección de acciones según respuesta
 if mcpremium:
